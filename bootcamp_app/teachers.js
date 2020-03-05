f22aeb4057c6b5d984776e6b39cb9a560367f9bd
Diogo Pinto <diogosmp@gmail.com>
2020-03-05 03:05:16 +0000

    Implemented Parametrized Queries using node-postgres'

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,19 +10,20 @@ const pool = new Pool(
   console.log("Connected")
 );
 
-pool
-  .query(
-    `SELECT teachers.name AS teacher, cohorts.name AS cohort
-  FROM teachers
-  JOIN assistance_requests ON teachers.id = teacher_id
-  JOIN students ON students.id = assistance_requests.student_id
-  JOIN cohorts ON cohorts.id = students.cohort_id
-  WHERE cohorts.name = '${process.argv.slice(2)[0].toUpperCase()}'
-  GROUP BY teachers.name, cohorts.name
-  ORDER BY teachers.name;`
-  )
-  .then(res => {
-    res.rows.forEach(row => {
-      console.log(`${row.cohort}: ${row.teacher}`);
-    });
+const queryString = `SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name = $1
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;`;
+
+const cohortName = process.argv[2] || "FEB12";
+const values = [cohortName];
+
+pool.query(queryString, values).then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   });
+});
